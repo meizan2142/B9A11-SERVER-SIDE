c67d6e3492f2f0e922f63eeab82552e174bc9ffc
Your Name01~git <saifsahai2104@gmail.com>
2024-05-14 09:51:57 +0600

    all jobs data of home page have been sent on client side

--- index.js
+++ index.js
@@ -1,11 +1,47 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000
 const app = express()
 require('dotenv').config()
 
-app.use(cors())
+const corsOptions = {
+    origin: ['http://localhost:5173'],
+    credentials: true,
+    optionSuccessStatus: 200
+}
+
+app.use(cors(corsOptions))
 app.use(express.json())
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.usv0l7z.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Collection
+        const jobCollection = client.db('JobInformations').collection('jobCategory')
+        // data of tab
+        app.get('/jobs', async(req, res) => {
+            const result = await jobCollection.find().toArray()
+            res.send(result)
+        })
+    } finally {
+    }
+}
+run().catch(console.dir);
+
 
 
 app.get('/', (req, res) => {
